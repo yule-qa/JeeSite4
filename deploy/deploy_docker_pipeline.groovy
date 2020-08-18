38d5f3111956ab9ebed735f14bc79043ce9fdf2f
christbella <308667244@qq.com>
2020-08-18 22:13:33 +0800

    jeestie

--- deploy/deploy_docker_pipeline.groovy
+++ deploy/deploy_docker_pipeline.groovy
@@ -15,7 +15,7 @@ pipeline {
     stages{
         stage('同步源码') {
             steps {
-                git url:'git@gitee.com:11547299/jeesite4.git', branch:'$branch'
+                git url:'https://github.com/yule-qa/JeeSite4.git', branch:'$branch'
             }
         }
 
